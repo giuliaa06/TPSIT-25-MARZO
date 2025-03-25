cb8c21bc56ac5ce71c94bbdfcb49afb691ae964a
lai2-24 <lai2-24@itismeucci.com>
2025-03-25 14:06:51 +0100

    aggiunta view, create, update, destroy

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -12,4 +12,47 @@ class AlunniController
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
+
+  public function view(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id = " . $args['id']);
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+    public function create(Request $request, Response $response, $args){
+      $body = json_decode($request->getBody()->getContents(), true);
+      
+      
+      $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+      $result = $mysqli_connection->query("INSERT INTO alunni (nome, cognome) VALUES ('" . $body['nome'] . "', '" . $body['cognome'] . "' )");
+      //$results = $result->fetch_all(MYSQLI_ASSOC);
+  
+      $response->getBody()->write(json_encode($results));
+      return $response->withHeader("Content-type", "application/json")->withStatus(200);
+    }
+
+    public function update(Request $request, Response $response, $args) {
+      $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+      $body = json_decode($request->getBody()->getContents(), true);
+      $id = $args['id']; 
+      $nome = $body["nome"];
+      $cognome = $body["cognome"];
+      $stmt = $mysqli_connection->prepare("UPDATE alunni SET nome = ?, cognome = ? WHERE id = ?");
+      $stmt->bind_param("ssi", $nome, $cognome, $id);
+      $stmt->execute();
+      
+      // Controllo
+      if ($stmt->affected_rows > 0) {
+          $response->getBody()->write(json_encode(["message" => "Alunno aggiornato con successo"]));
+          return $response->withHeader("Content-type", "application/json")->withStatus(200);
+      } else {
+          // Se non è stato aggiornato nulla (ad esempio, l'ID non esiste)
+          $response->getBody()->write(json_encode(["message" => "Nessun alunno trovato con l'ID fornito"]));
+          return $response->withHeader("Content-type", "application/json")->withStatus(404);
+      }
+  } 
+
+
 }
--- php/index.php
+++ php/index.php
@@ -7,5 +7,9 @@ require __DIR__ . '/controllers/AlunniController.php';
 $app = AppFactory::create();
 
 $app->get('/alunni', "AlunniController:index");
+$app->get('/alunni/{id}', "AlunniController:view");
+$app->post('/alunni/create', "AlunniController:create");
+$app->put('/alunni/update/{id}', "AlunniController:update");
+$app->delete('/alunni/destroy/{id}', "AlunniController:destroy");
 
 $app->run();
